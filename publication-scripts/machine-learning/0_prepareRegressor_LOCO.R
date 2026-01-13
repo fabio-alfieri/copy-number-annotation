23eb2eeeeae50e6a2877bc6dde98194c974f9a96
Fabio Alfieri <54310352+fabio-alfieri@users.noreply.github.com>
2026-01-13 13:32:47 -0500

    Update 0_prepareRegressor_LOCO.R

--- publication-scripts/machine-learning/0_prepareRegressor_LOCO.R
+++ publication-scripts/machine-learning/0_prepareRegressor_LOCO.R
@@ -1,5 +1,7 @@
-hyperTuning_script <- './1_hyperTuning_LOCO.R'
-runRegressor_script <- './2_runRegressor_LOCO.R'
+wd <- 'path/to/GitHub/copy-number-annotation/'
+
+hyperTuning_script <- 'publication-scripts/machine-learning/1_hyperTuning_LOCO.R'
+runRegressor_script <- 'publication-scripts/machine-learning/2_runRegressor_LOCO.R'
 
 chr_nums <- as.factor(unlist(lapply(X = ml_table$bin, FUN = function(x){
   chr_number <- strsplit(x, split = "_")[[1]][[1]]
@@ -9,15 +11,15 @@ chromosomes <- sort(as.numeric(levels(chr_nums)))
 
 for(chromosome in chromosomes){
   
-hyperTuning_path <- paste0('../Data/hyperparam_tuning/', classS, '_hyper_tuning_LOCO_', chromosome,'.rds')
+hyperTuning_path <- paste0(wd, 'data/hyperparam_tuning/', classS, '_hyper_tuning_LOCO_', chromosome,'.rds')
 
-source(hyperTuning_script, local = T)
+source(paste0(wd, hyperTuning_script), local = T)
 
 hyper_tuning <- readRDS(file = hyperTuning_path)
 
 params <- hyper_tuning$results[order(hyper_tuning$results$RMSE, decreasing = F),]
 params <- as.list(params[1,-c(7:13)])
 
-source(runRegressor_script, local = T)
+source(paste0(wd, runRegressor_script), local = T)
 
 }
