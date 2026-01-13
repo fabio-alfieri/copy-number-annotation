4b05ffcabab758ff8bf010392aec87f7646dffe4
Fabio Alfieri <54310352+fabio-alfieri@users.noreply.github.com>
2026-01-13 13:31:05 -0500

    Update 0_prepareRegressor_LAFO_PROXY.R

--- publication-scripts/machine-learning/0_prepareRegressor_LAFO_PROXY.R
+++ publication-scripts/machine-learning/0_prepareRegressor_LAFO_PROXY.R
@@ -1,5 +1,7 @@
-hyperTuning_script <- './1_hyperTuning_LAFO_PROXY.R'
-runRegressor_script <- './2_runRegressor_LAFO_PROXY.R'
+wd <- 'path/to/GitHub/copy-number-annotation/'
+
+hyperTuning_script <- 'publication-scripts/machine-learning/1_hyperTuning_LAFO_PROXY.R'
+runRegressor_script <- 'publication-scripts/machine-learning/2_runRegressor_LAFO_PROXY.R'
 
 features_to_keep <- c("Centromere_Length", 
                       "Chromosome_Length", 
@@ -9,11 +11,11 @@ features_to_keep <- c("Centromere_Length",
 
 for(feature_to_keep in features_to_keep){
   
-  hyperTuning_path <- paste0('../Data/hyperparam_tuning/', classS, '_hyper_tuning_LAFO_PROXY_', feature_to_keep,'.rds')
+  hyperTuning_path <- paste0(wd, 'data/hyperparam_tuning/', classS, '_hyper_tuning_LAFO_PROXY_', feature_to_keep,'.rds')
   
   features_to_discard <- features_to_keep[features_to_keep != feature_to_keep]
   
-  source(hyperTuning_script, local = T)
+  source(paste0(wd, hyperTuning_script), local = T)
   
   hyper_tuning <- readRDS(file = hyperTuning_path)
   
@@ -21,6 +23,6 @@ for(feature_to_keep in features_to_keep){
   params <- hyper_tuning$results[order(hyper_tuning$results$RMSE, decreasing = F),]
   params <- as.list(params[1,-c(7:13)])
   
-  source(runRegressor_script, local = T)
+  source(paste0(wd, runRegressor_script), local = T)
   
 }
