88829d054fe0df6bc0ad51ceff07a17ed954c32e
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2026-01-18 11:45:01 +0100

    Refactor file paths and install packages dynamically

--- publication-scripts/feature-matrix/compute-features/02_Feature_preprocess_ohnologs_paralogs.R
+++ publication-scripts/feature-matrix/compute-features/02_Feature_preprocess_ohnologs_paralogs.R
@@ -1,25 +1,39 @@
-
 #Pre-processing of feature data
 #Ohnologs and paralogs
 #Tissue-general
 
-# Set working directory 
-setwd("/mnt/fabiogokce/gokce")
+packages <- c(
+  "readxl", "dplyr"
+)
 
-#Required libraries
-library(readxl)
-library(dplyr)
+installed <- rownames(installed.packages())
+for (pkg in packages) {
+  if (!pkg %in% installed) {
+    install.packages(pkg, dependencies = TRUE)
+  }
+}
+
+lapply(packages, library, character.only = TRUE)
+
+ohnologs_path_makino <- "./Downloads/Ohnologs/st01.xls"
+ohnologs_path_ensembl <- "./Downloads/ensembl_release56.txt"
+ohnolog_database_path <- "./Downloads/Ohnologs"
+ohnolog_list_path <- "./Data/OhnologList.RData"
+
+paralogs_path <- "./Downloads/Table_S8.csv"
+paralog_outpath <- "./Data/ParalogList.RData"
 
 ohnologs <- list()
 
 #Ohnologs from different datasets
 #1. From Makino and Mclysaght paper
-makino_mclysaght <- read_excel("./Downloads/Ohnologs/st01.xls", sheet = 5, skip = 1, col_types = "text")
+makino_mclysaght <- read_excel(ohnologs_path_makino, sheet = 5, skip = 1, col_types = "text")
 makino_mclysaght <- makino_mclysaght[-c(9058:9060),]
+
 #The ohnologs are provided with their ensembl gene IDs, and ensembl release 52 was used on the paper.
 #However, that version is not available anymore. 
 #Therefore, Ensembl release 54 was used to convert IDs to symbols. 
-ensembl_release56 <- read.delim("./Downloads/ensembl_release56.txt")
+ensembl_release56 <- read.delim(ohnologs_path_ensembl)
 makino_mclysaght <- merge(makino_mclysaght,ensembl_release56,by.x = "Ohnolog1",by.y="Ensembl.Gene.ID")
 colnames(makino_mclysaght)[13] <- "Associated.Gene.Name1"
 makino_mclysaght <- merge(makino_mclysaght,ensembl_release56,by.x = "Ohnolog2",by.y="Ensembl.Gene.ID")
@@ -40,8 +54,9 @@ ohno <- ohno %>% group_by(Symbol) %>%
             "n_ohnolog_mmpaper" = sum(n_ohnolog_mmpaper)) #7290
 #Add the data to the final list
 ohnologs[["mmpaper"]] <- ohno
+
 #2. From Ohnolog database (different cutoffs)
-files <- list.files(path = "./Downloads/Ohnologs", pattern = "^hsapiens.Pairs*", full.names = T, recursive = F)
+files <- list.files(path = ohnolog_database_path, pattern = "^hsapiens.Pairs*", full.names = T, recursive = F)
 for(file in files){
   cutoff <- strsplit(basename(file),"\\.")[[1]][3]
   ohnodatabase <- read.delim(file)
@@ -60,12 +75,12 @@ for(file in files){
     summarise("ohnologs" = paste(ohnologs,collapse = "::"),
               "n_ohnolog" = sum(n_ohnolog))
   ohnologs[[paste("OhnoDatabase",cutoff,sep = "_")]] <- ohno}
-save(ohnologs, file = "./Data/OhnologList.RData")
+save(ohnologs, file = ohnolog_list_path)
 
 #Paralogs
 #1. Colm' data
 #paralogs <- read.csv("./Downloads/all_paralog_genes_min20.csv") #Paralogs
-paralog.features <- read.csv("./Downloads/Table_S8.csv") #Detailed features for paralogs
+paralog.features <- read.csv(paralogs_path) #Detailed features for paralogs
 #Colm's cut-off for sequence identity (reciprocal, at least 20%)
 paralog.features <- paralog.features[paralog.features$min_sequence_identity >= 0.20,3:5]
 paralog.genes <- union(paralog.features$A1, paralog.features$A2)
@@ -77,5 +92,5 @@ for(gene in paralog.genes){
 }
 paralogs <- as.data.frame(paralogs)
 colnames(paralogs) <- c("symbol","paralogs","n_paralogs")
-save(paralogs, file = "./Data/ParalogList.RData")
+save(paralogs, file = paralog_outpath)
 
